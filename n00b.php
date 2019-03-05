40ced40c67936e01b0a4ba01a1dfe0e825666cdf
Peter Rodgath <rodgath@gmail.com>
2019-03-05 04:22:19 +0300

    Created n00b.php main file.

---
+++ n00b.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Plugin Name: [Plugin Name]
+ * Plugin URI:  [http://link to your plugin homepage]
+ * Description: [Sample plugin description]
+ * Version:     1.0
+ * Author:      [Plugin Author Name]
+ * Author URI:  [http://plugin-author-url.tld]
+ * Text Domain: [plugin-name]
+ * Domain Path: /languages
+ * License:		GPL-2.0+
+ * License URI:	http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * This program is free software; you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License version 2, 
+ * as published by the Free Software Foundation. You cannot use any 
+ * other versions of the License except version 2 or later.
+ *
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+defined('ABSPATH') || exit('No Dice!');
