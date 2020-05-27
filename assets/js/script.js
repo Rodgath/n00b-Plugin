ac28f3cd71bcd9d6d9103d29fada9646ba75eec9
Rodgath <rodgath@gmail.com>
2020-05-27 17:20:12 +0300

    Plugin script localization example

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,11 @@ jQuery(document).ready(function($){
 
 	'use strict';
 	
-	/* Your code starts from here */
+	/* -------------------------- */
+	/* Your code starts from here */	
+	/* -------------------------- */
 	
+	/* How to get site url using plugin localization - l10n */
+	var siteURL = n00b_l10n.site_url;
 	
 });
