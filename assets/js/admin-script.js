9d2edd7a717a45ae0e92a0e180da0f303f18d899
Rodgath <rodgath@gmail.com>
2020-05-27 17:21:00 +0300

    Plugin script localization example

--- assets/js/admin-script.js
+++ assets/js/admin-script.js
@@ -4,7 +4,11 @@ jQuery(document).ready(function($){
 
 	'use strict';
 	
-	/* Your code starts from here */
+	/* -------------------------- */
+	/* Your code starts from here */	
+	/* -------------------------- */
 	
+	/* How to get site url using plugin localization - l10n */
+	var adminURL = n00b_l10n.admin_url;
 	
 });
