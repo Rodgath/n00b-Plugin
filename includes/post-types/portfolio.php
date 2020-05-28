d46b4761a8aa7aace7b717a0dba51751537a9389
Rodgath <rodgath@gmail.com>
2020-05-28 19:55:41 +0300

    Add files via upload

--- includes/post-types/portfolio.php
+++ includes/post-types/portfolio.php
@@ -1,7 +1,12 @@
 <?php
 
 /* Hook into 'init' action to register the post type */
-add_action( 'init', 'n00b_portfolio_post_type' );
+add_action('init', 'n00b_portfolio_post_type');
 
 /* Function to create the custom post type */
-function n00b_portfolio_post_type() {}
+function n00b_portfolio_post_type() {
+	
+	$args = array();
+	
+	register_post_type('portfolio', $args);
+}
